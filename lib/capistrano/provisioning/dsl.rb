65c58b4687cc13ae0e51dfdb8c0ae1fc980d3d68
Bruno Zanchet <bzanchet@gmail.com>
2016-03-25 12:49:18 -0300

    adding support for gems

--- lib/capistrano/provisioning/dsl.rb
+++ lib/capistrano/provisioning/dsl.rb
@@ -7,6 +7,7 @@ module Capistrano
         @context = context
       end
 
+      # TODO: skip if md5sum matches
       def file(path, options = {})
         owner = options[:owner] || "root"
         cmd = "sudo -u #{owner} mkdir -p #{File.dirname(path)}"
@@ -20,6 +21,12 @@ module Capistrano
           cmd = "sudo chmod #{mode} #{path}"
           execute(cmd)
         end
+      end
+
+      def gem(gem)
+        return if gem_installed?(gem)
+        cmd = "sudo gem install #{gem} --no-document --quiet"
+        execute(cmd)
       end
 
       def package(package, options = {})
@@ -53,6 +60,10 @@ module Capistrano
 
       def execute(cmd)
         @context.execute(cmd)
+      end
+
+      def gem_installed?(gem)
+        test("gem query -i #{gem} > /dev/null")
       end
 
       def group_installed?(group)
