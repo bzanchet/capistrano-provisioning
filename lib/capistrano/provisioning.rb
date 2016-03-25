60ca3284226f181b4ba12ec2825adff43c267bf0
Bruno Zanchet <bzanchet@gmail.com>
2016-03-25 11:38:08 -0300

    gem scaffold by bundler

---
+++ lib/capistrano/provisioning.rb
@@ -0,0 +1,7 @@
+require "capistrano/provisioning/version"
+
+module Capistrano
+  module Provisioning
+    # Your code goes here...
+  end
+end
---
+++ lib/capistrano/provisioning/version.rb
@@ -0,0 +1,5 @@
+module Capistrano
+  module Provisioning
+    VERSION = "0.1.0"
+  end
+end
